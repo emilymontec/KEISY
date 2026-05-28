8bc338865634f5847a3e2da1ae14a162abb83194
emilymontec <emilymonterrosa.c@gmail.com>
2026-05-27 22:08:56 -0500

    Actualizacion; modules

--- KEISY/settings.py
+++ KEISY/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authentication',
+    'patients',
+    'etl',
+    'analytics',
+    'ml',
+    'dashboard',
+    'reports'
 ]
 
 MIDDLEWARE = [
@@ -115,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
