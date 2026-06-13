360129c6025c0be9174df30669cffb49f2a01712
emilymontec <emilymonterrosa.c@gmail.com>
2026-06-12 22:34:41 -0500

    Correccion; ml module

--- ml/urls.py
+++ ml/urls.py
@@ -3,7 +3,7 @@ from .views import ml_dashboard, predict_individual, train_model_view
 
 
 urlpatterns = [
-    path("dashboard/", ml_dashboard, name="ml_dashboard"),
+    path("ml/", ml_dashboard, name="ml_dashboard"),
     path("predict/", predict_individual, name="predict_individual"),
     path("train/", train_model_view, name="train_model"),
 ]
